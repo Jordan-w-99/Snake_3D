106bec19c0e63b7ca5bf58112d4e7c30774d1e2c
Jordan <jordanw99@hotmail.co.uk>
2022-04-09 23:45:23 +0100

    move snake into class

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { Vector3 } from 'three';
+import Snake from './snake';
 
 const scene = new THREE.Scene();
 
@@ -39,69 +39,24 @@ ground.position.setZ(halfBoardSize - halfSegmentSize);
 ground.rotateX(-Math.PI / 2);
 scene.add(ground);
 
-let snake = []; // array of coordinates & headings, only get incremented in units of 1
-let snakeMesh = []; // array of mesh objects, position these using snake array, heading & progress
-
-const head = new THREE.Vector3(Math.floor(halfBoardSize), 0, Math.floor(halfBoardSize));
-head.heading = new THREE.Vector3(0, 0, 0);
-snake.push(head);
-createSegment();
+const snake = new Snake(scene, segmentSize, halfBoardSize);
 
 let food;
 createNewFood();
 
-let newHeading = new THREE.Vector3(0, 0, 0);
-let progress = 0;
-
 function update() {
   renderer.render(scene, camera);
 
-
-  progress += 0.05;
-  if (progress > 1) {
-    shiftSnake();
-    progress = 0;
+  // console.log(snake.newHeading);
+  snake.update();
+  if(snake.isEating(scene, food)) {
+    createNewFood();
   }
-
-  updateSnakeMesh();
-
-  isEating();
 
   requestAnimationFrame(update);
 };
 
 update();
-
-function updateSnakeMesh() {
-  for (let i = 0; i <= snakeMesh.length - 1; i++) {
-    const xPos = snake[i].x + snake[i].heading.x * progress;
-    const zPos = snake[i].z + snake[i].heading.z * progress;
-    snakeMesh[i].position.setX(xPos);
-    snakeMesh[i].position.setZ(zPos);
-  }
-}
-
-function shiftSnake() {
-  const newSegments = snake;
-  for (let i = snake.length - 1; i >= 1; i--) {
-    newSegments[i].x = newSegments[i - 1].x;
-    newSegments[i].z = newSegments[i - 1].z;
-    newSegments[i].heading.copy(newSegments[i - 1].heading);
-  }
-
-  newSegments[0].x += newSegments[0].heading.x;
-  newSegments[0].z += newSegments[0].heading.z;
-
-  snake = newSegments;
-  snake[0].heading.copy(newHeading);
-}
-
-function isEating() {
-  if (snake[0].distanceTo(food.position) < 0.1) {
-    createNewFood();
-    growSnake();
-  }
-}
 
 function createNewFood() {
   const x = THREE.MathUtils.randInt(0, boardSize - 1);
@@ -116,23 +71,6 @@ function createNewFood() {
   scene.remove(food);
   food = newFood;
   scene.add(newFood);
-}
-
-function growSnake() {
-  const newSegment = (new THREE.Vector3()).copy(snake[snake.length - 1]);
-  // newSegment.sub(snake[snake.length - 1].heading)
-  newSegment.heading = (new Vector3()).copy(snake[snake.length - 1].heading);
-  snake.push(newSegment);
-  createSegment();
-}
-
-function createSegment() {
-  const segment = new THREE.Mesh(
-    new THREE.BoxGeometry(segmentSize, segmentSize, segmentSize),
-    new THREE.MeshStandardMaterial({ color: 0xFF0000 })
-  );
-  snakeMesh.push(segment);
-  scene.add(segment);
 }
 
 window.addEventListener('keypress', e => {
@@ -156,7 +94,7 @@ window.addEventListener('keypress', e => {
       break;
   }
 
-  if (h.x != snake[0].heading.x * -1 || h.z != snake[0].heading.z * -1) {
-    newHeading = h;
+  if (h.x != snake.segments[0].heading.x * -1 || h.z != snake.segments[0].heading.z * -1) {
+    snake.newHeading = h;
   }
 })
---
+++ snake.js
@@ -0,0 +1,74 @@
+import * as THREE from 'three';
+
+export default class Snake {
+    constructor(scene, segmentSize, halfBoardSize) {
+        this.segments = []; // array of coordinates & headings, only get incremented in units of 1
+        this.mesh = []; // array of mesh objects, position these using snake array, heading & progress
+        this.animationProgress = 0;
+        this.newHeading = new THREE.Vector3(0, 0, 0);
+        this.segmentSize = segmentSize;
+
+        const head = new THREE.Vector3(Math.floor(halfBoardSize), 0, Math.floor(halfBoardSize));
+        head.heading = new THREE.Vector3(0, 0, 0);
+        this.segments.push(head);
+        this.createSegment(scene);
+    }
+
+    update() {
+        this.animationProgress += 0.05;
+        if (this.animationProgress > 1) {
+            this.shiftSnake();
+            this.animationProgress = 0;
+        }
+
+        this.updateSnakeMesh();
+    }
+
+    updateSnakeMesh() {
+        for (let i = 0; i <= this.mesh.length - 1; i++) {
+            const xPos = this.segments[i].x + this.segments[i].heading.x * this.animationProgress;
+            const zPos = this.segments[i].z + this.segments[i].heading.z * this.animationProgress;
+            this.mesh[i].position.setX(xPos);
+            this.mesh[i].position.setZ(zPos);
+        }
+    }
+
+    shiftSnake() {
+        const newSegments = this.segments;
+        for (let i = this.segments.length - 1; i >= 1; i--) {
+            newSegments[i].x = newSegments[i - 1].x;
+            newSegments[i].z = newSegments[i - 1].z;
+            newSegments[i].heading.copy(newSegments[i - 1].heading);
+        }
+
+        newSegments[0].x += newSegments[0].heading.x;
+        newSegments[0].z += newSegments[0].heading.z;
+
+        this.segments = newSegments;
+        this.segments[0].heading.copy(this.newHeading);
+    }
+
+    isEating(scene, food) {
+        if (this.segments[0].distanceTo(food.position) < 0.1) {
+            this.growSnake(scene);
+            return true;
+        }
+        return false;
+    }
+
+    growSnake(scene) {
+        const newSegment = (new THREE.Vector3()).copy(this.segments[this.segments.length - 1]);
+        newSegment.heading = (new THREE.Vector3()).copy(this.segments[this.segments.length - 1].heading);
+        this.segments.push(newSegment);
+        this.createSegment(scene);
+    }
+
+    createSegment(scene) {
+        const segment = new THREE.Mesh(
+            new THREE.BoxGeometry(this.segmentSize, this.segmentSize, this.segmentSize),
+            new THREE.MeshStandardMaterial({ color: 0xFF0000 })
+        );
+        this.mesh.push(segment);
+        scene.add(segment);
+    }
+}
